6f4b2735ca36b0fbd59c923ed815b27e5e029b99
proyectoKR <k.r.c.1000000@gmail.com>
2024-10-31 20:54:23 -0600

    Add files via upload

---
+++ Pruebas/archivo.js
@@ -0,0 +1,26 @@
+//operadores de asignacion, incremento y decremento
+
+let a = 1;
+
+document.write('valor de a: ', a);
+document.write('<br>');
+a++;//aumenta valor en 1
+document.write('valor de a: ', a);
+document.write('<br>');
+a+=5;//aumento variable
+document.write('valor de a: ',a);
+document.write('<br>');
+a--;// resta valor en 1
+document.write('valor de a: ',a);
+document.write('<br> ');
+a-=3;// resta variable
+document.write('valor de a: ',a);
+document.write('<br> ');
+a*=2;//a*a
+document.write(' valor de a: ',a);
+document.write('<br> ');
+a/=3;//a/a
+document.write('valor de a:  ',a);
+document.write('<br> ');
+a**=2;// elevar la potencia
+document.write('valor de a: ',a);
